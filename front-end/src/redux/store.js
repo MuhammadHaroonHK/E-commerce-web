71ae8ebfa165cb8d4ed31be3f6f5188a1a217b55
MuhammadHaroonHK <haroonhk059@gmail.com>
2025-05-26 09:13:24 +0500

    Checkout Reducer added

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice';
 import productsReducer from './slices/productSlice'
 import cartReducer from './slices/cartSlice'
+import checkoutReducer from './slices/checkoutSlice'
 const store=configureStore({
     reducer:{
         auth: authReducer,
         products:productsReducer,
         cart:cartReducer,
+        checkout:checkoutReducer,
     },
 });
 
